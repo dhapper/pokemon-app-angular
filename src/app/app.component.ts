3ec80a56d3a8cff7cd8fc9f811293558411eea95
dhapper <dhaarshan.p@gmail.com>
2024-10-04 00:40:32 -0400

    Added rough card and layout component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./components/card/card.component";
-import { provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { CardLayoutComponent } from "./components/card-layout/card-layout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [
+    RouterOutlet,
+    CardComponent,
+    CommonModule,
+    CardLayoutComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: []
 })
 export class AppComponent {
   title = 'pokemon_app';
+
+  renderedIndex: number[] = [];
+
+  ngOnInit(): void {
+    this.renderedIndex = Array.from({ length: 151 }, (_, i) => i + 1); // Generates an array from 1 to 151
+  }
+
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,30 +1,33 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Input } from '@angular/core';
 import { PokeAPIService } from '../../services/poke-api.service';
 import { Root } from '../../model/interface/pokemon-info';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent implements OnInit{
 
-  pokemon: Root = {} as Root; // Initialize as an empty object
+  @Input() nameOrId: string = '';
 
+  pokemon: Root | null = null; 
   pokeAPIService = inject(PokeAPIService);
 
   ngOnInit(): void {
-    this.getPokemonData("1"); // Call the method with a Pokémon name or ID
+      this.getPokemonData(this.nameOrId);
   }
   
   getPokemonData(nameOrId: string): void {
     this.pokeAPIService.getPokemon(nameOrId).subscribe(
       data => {
         this.pokemon = data; // Store the response in the pokemon object
-        console.log(this.pokemon); // Log the data to see its structure
       },
       error => {
         console.error('Error fetching Pokémon data:', error); // Handle errors
