c68f8804bfa69386b98ea9afce4f92758ed88596
dhapper <dhaarshan.p@gmail.com>
2024-12-04 20:10:57 -0500

    Update GitHub Pages with the latest build

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
+  constructor(private router: Router) {}
+
+  navigateToGen(gen: number) {
+    this.router.navigate([`/dex/${gen}`]);
+  }
 }
